be4c9ab3be7f7cda93cdd0c120b588bfb2d81ba6
brianjo94 <brianjo1994@gmail.com>
2023-09-20 12:35:54 -0400

    JS codes to review and study

---
+++ codesToReview.js
@@ -0,0 +1,45 @@
+
+/* Code 1 to review, using anonymous function as argument.
+function map(f, a) {
+    const result = new Array(a.length);
+    for (let i = 0; i < a.length; i++) {
+      result[i] = f(a[i]);
+    }
+    return result;
+  }
+  
+  const cube = function (x) {
+    return x * x * x;
+  };
+  
+  const numbers = [0, 1, 2, 5, 10];
+  console.log(map(cube, numbers)); // [0, 1, 8, 125, 1000]
+  */
+
+
+
+  // eval() function?
+  //call and apply method on a function
+
+
+
+  
+  function foo(i) {
+    if (i < 0) {
+      return;
+    }
+    console.log(`begin: ${i}`);
+    foo(i - 1);
+    console.log(`end: ${i}`);
+  }
+  foo(3);
+  
+  // Logs:
+  // begin: 3
+  // begin: 2
+  // begin: 1
+  // begin: 0
+  // end: 0
+  // end: 1
+  // end: 2
+  // end: 3
