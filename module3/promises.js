41f585f8c33fee1d677a435809b113d625681954
brianjo94 <brianjo1994@gmail.com>
2023-10-01 22:49:25 -0400

    how promises work

--- module3/promises.js
+++ module3/promises.js
@@ -9,3 +9,15 @@ let promise = new Promise((resolve, reject) => {
   });  
   promise.then((fromRes) => console.log(fromRes)). 
     catch((fromRej) => console.log(fromRej));
+
+  /*
+A promise is created using a constructor that takes a call back function with two arguments (line 1).
+
+The code needed to perform the promised task is written. In this example, it is assumed that the code executes successfully (line 2).
+
+If the task is successful, the promise is resolved. In this example, the optional parameter “The promised task was performed successfully” is passed (lines 4-5).
+
+If the task is unsuccessful, then the promise is rejected. In this example, an optional parameter is passed (lines 6-7).
+
+The then() method is called when the promise is resolved, ​and the catch() method is called if the promise is rejected or if there was an error during the code execution (lines 10-11).
+*/
