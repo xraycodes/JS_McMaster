a5e7484ac3e87978101e9b8fcadc1cecae3c2631
brianjo94 <brianjo1994@gmail.com>
2023-10-11 14:22:22 -0400

    adding and deleting properties in js objects

--- module3/objects_YT.js
+++ module3/objects_YT.js
@@ -34,3 +34,14 @@ function Circle(radius) {
 }
 
 const another = new Circle(5);
+
+//dynamically adding properties
+another.location = {x: 1};
+console.log(another.location)
+console.log(another['location'])  // use bracket notation to access value, good for if key has space or - or special char
+//or you can also declare a variable and just pass that in the bracket
+
+//deleting property 
+console.log(another.radius)
+delete another.radius // can use delete another['radius']
+
