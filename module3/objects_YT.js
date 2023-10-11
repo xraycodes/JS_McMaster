d1c1bbe48a443a8ea70a9fe1f81b55dbcdecd23e
brianjo94 <brianjo1994@gmail.com>
2023-10-11 14:42:32 -0400

    iterating over values and keys in objects

--- module3/objects_YT.js
+++ module3/objects_YT.js
@@ -22,8 +22,7 @@ function createCircle(radius) {
 }
 
 const circles = createCircle(1);
-console.log(circles.radius)
-circles.draw()
+
 
 //constructor function
 function Circle(radius) {
@@ -37,11 +36,24 @@ const another = new Circle(5);
 
 //dynamically adding properties
 another.location = {x: 1};
-console.log(another.location)
-console.log(another['location'])  // use bracket notation to access value, good for if key has space or - or special char
+// console.log(another.location)
+// console.log(another['location'])  // use bracket notation to access value, good for if key has space or - or special char
 //or you can also declare a variable and just pass that in the bracket
 
 //deleting property 
-console.log(another.radius)
+console.log(another.location)
 delete another.radius // can use delete another['radius']
 
+//enumerate/iterate over properties
+for (let key in another) {
+    console.log(key, circle[key]);
+}
+
+//to get all the keys
+const keys = Object.keys(another);
+console.log(keys)
+
+//check if object has given property
+if ('location' in another) {
+    console.log('yes');
+}
