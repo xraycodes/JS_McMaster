91dd37bc5b65262ad93286805448644330a8563d
brianjo94 <brianjo1994@gmail.com>
2023-10-26 19:24:17 -0400

    using vue with CLI

---
+++ module5/vue-project/src/App.vue
@@ -0,0 +1,36 @@
+<script>
+
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      title: 'My first Vue App'
+    }
+  },
+  methods: {
+    HandleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add()
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <modal/>
+  <input type="text" ref="name">
+  <button @click="HandleClick">Click me</button>
+</template>
+
+<style>
+h1 {
+  color: red;
+}
+
+</style>
---
+++ module5/vue-project/src/components/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <p>modal contens</p>
+        </div>
+    </div>
+</template>
+
+<style>
+.modal {
+    width: 400px;
+    background: white;
+    padding:20px;
+    border-radius: 10px;
+    margin: 100px auto;
+
+}
+
+.backdrop {
+    top:0;
+    position:fixed;
+    background: rgba(red, green, blue, alpha);
+    width:100px;
+    height:100px
+}
+</style>
