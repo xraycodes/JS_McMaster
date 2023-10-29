e9aba91f3ed3817c1414b679ae2c8bd208ac8424
brianjo94 <brianjo1994@gmail.com>
2023-10-28 22:29:56 -0400

    using methods in vue instance instead of reactivity, ref

---
+++ module5/practice/practice/src/App.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    data() {
+        return {
+            tempInCelcius: 0,
+            tempInFahrenheit: 32,
+            isValidNumber: true
+        }
+    },
+    methods: {
+        // Both methods rounds to nearest whole number
+        celciusToFarenheit() {
+                this.tempInFahrenheit = ((this.tempInCelcius * 9/5) + 32).toFixed()
+        },
+        farenheitToCelcius() {
+            this.tempInCelcius = ((this.tempInFahrenheit - 32) * 5/9).toFixed()
+        },
+    }
+} 
+</script>
+
+<template>
+<h1>Welcome to temperature converter app</h1>
+<br>
+
+<!-- Converts C to F using function upon input event listener -->
+<label for="celcius">Degree in celcius is </label>
+<input type="text" id="celcius" @input="celciusToFarenheit()" placeholder="Place a valid number" v-model="tempInCelcius" >
+
+<!-- Converts F to C using function upon input event listener -->
+<label for="fahrenheit"> degree in fahrenheit </label>
+<input type="text" id="fahrenheit" @input="farenheitToCelcius()" placeholder="Place a valid number" v-model="tempInFahrenheit">
+
+<!-- Creates a p tag if any of the inputs are not numbers -->
+<p v-if="isNaN(tempInCelcius) || isNaN(tempInFahrenheit)" :style="{color:'red', fontWeight:'bold'}">Enter valid number</p>
+</template>
