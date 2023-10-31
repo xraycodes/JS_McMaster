4b8a12bd13d76b8027b990891b646d76a03325cf
brianjo94 <brianjo1994@gmail.com>
2023-10-31 11:54:11 -0400

    computed properties example, filter

--- module5/vue_practice/vue_practice/src/vuePractice.vue
+++ module5/vue_practice/vue_practice/src/vuePractice.vue
@@ -1,10 +1,14 @@
 <script>
-import { ref,reactive } from 'vue';
+import { ref,reactive, computed } from 'vue';
 
 export default {
     setup() {
         const hello = ref('greeting')
         const para = ref(23)
+
+        const search = ref('')
+
+        const names = ref(['bob', 'billy', 'joel','david','peter','kim','lucy'])
 
         const update = reactive({name: 'Brian', age: 29})
 
@@ -18,16 +22,22 @@ export default {
 
         const updateInfo = () => {
             update.name = 'Jimmy'
-            update.age = '20'
+            update.age = '20' 
         }
 
-        return{hello, para, handleClick, update,updateInfo}
+        const matchingNames = computed(() => {
+            return names.value.filter((name) => name.includes(search.value))
+        })
+
+        return{hello, para, handleClick, update,updateInfo, matchingNames, names, search}
     }
 }
 
 </script>
 
 <template>
+<p v-for="name in matchingNames">{{ name }}</p>
+<input type="text" v-model="search">
 <p >{{ hello }} brian you are {{ para  }} years old</p>
 <button @click="handleClick">click me</button>
 <input type="text" v-model="hello">
