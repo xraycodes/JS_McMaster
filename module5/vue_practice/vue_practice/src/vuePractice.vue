2fd3ffaa0b4103175d667456110803e3345e8e5a
brianjo94 <brianjo1994@gmail.com>
2023-10-31 10:37:46 -0400

    composition api, using ref in templates to add class

--- module5/vue_practice/vue_practice/src/vuePractice.vue
+++ module5/vue_practice/vue_practice/src/vuePractice.vue
@@ -1,45 +1,29 @@
-<script setup>
-import { ref, reactive, computed } from 'vue'
-import { duma } from './main'
+<script>
+import { ref } from 'vue';
 
-const message = 'hello world'
-const rawHTML = 'my name is bob'
-const truth = true
-const objectOfAttrs = {
-  id: 'bob',
-  class: 'wrapper'
+export default {
+    setup() {
+        const hello = 'greeting'
+        const para = ref(23)
+
+        const handleClick = () => {
+            console.log(para, para.value)
+            para.value.classList.add('test')
+            para.value.textContent = ('wagwan')
+        }
+
+        return{hello, para, handleClick}
+    }
 }
 
-const wordCount = ref('')
-const counter = computed(() => {
-  return wordCount.value.length
-})
- 
-
-const state = reactive({count: 0})
-
-const ob = ref(0)
 </script>
 
 <template>
-<input type="text" name="" id="" v-model="wordCount">
-<p> {{ counter }}/200</p>
-<button @click="duma.age++">{{duma.name}}</button>
-<h2>{{ duma.age }}</h2>
-<h1>{{ message }}</h1>
-<h1 id="bob" v-html="rawHTML" :id="bob"></h1>
-<h1 :disabled="truth">hello</h1>
-<div :="objectOfAttrs">sdsd</div>
-<p v-if="truth">hello v-if</p>
- <p>{{ ob }}</p>
- <p>state count{{ state.count }}</p>
- <button @click="state.count++">increment</button>
+<p ref="para">{{ hello}} brian</p>
+<button @click="handleClick">click me</button>
+
 </template>
 
 <style scoped>
 
-
-#bob {
-  color:red;
-}
 </style>
