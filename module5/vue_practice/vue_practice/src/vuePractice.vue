7f7a73074c5ee7df9debf5036bf2815da7925109
brianjo94 <brianjo1994@gmail.com>
2023-10-31 11:38:14 -0400

    ref and reactive using composition api

--- module5/vue_practice/vue_practice/src/vuePractice.vue
+++ module5/vue_practice/vue_practice/src/vuePractice.vue
@@ -1,26 +1,39 @@
 <script>
-import { ref } from 'vue';
+import { ref,reactive } from 'vue';
 
 export default {
     setup() {
-        const hello = 'greeting'
+        const hello = ref('greeting')
         const para = ref(23)
 
+        const update = reactive({name: 'Brian', age: 29})
+
         const handleClick = () => {
-            console.log(para, para.value)
-            para.value.classList.add('test')
-            para.value.textContent = ('wagwan')
+            // console.log(para, para.value)
+            // para.value.classList.add('test')
+            // para.value.textContent = ('wagwan')
+            hello.value = 'sweg'
+            para.value = 50
         }
 
-        return{hello, para, handleClick}
+        const updateInfo = () => {
+            update.name = 'Jimmy'
+            update.age = '20'
+        }
+
+        return{hello, para, handleClick, update,updateInfo}
     }
 }
 
 </script>
 
 <template>
-<p ref="para">{{ hello}} brian</p>
+<p >{{ hello }} brian you are {{ para  }} years old</p>
 <button @click="handleClick">click me</button>
+<input type="text" v-model="hello">
+<h1>Using reactive</h1>
+<h1> name is {{ update.name }} and age is {{ update.age }} </h1>
+<button @click="updateInfo">Reactive change</button>
 
 </template>
 
