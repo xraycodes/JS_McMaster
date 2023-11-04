0074ddd6dd1ef08042d7be46ac63c5aeb4dc3e5d
brianjo94 <brianjo1994@gmail.com>
2023-11-04 11:01:05 -0400

    importing json in vue and iterating over values

---
+++ toyEmporiumProject/toyEmporiumQuiz/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import JSONfile from './assets/questions.json'
+
+
+
+
+</script>
+
+<template>
+  <h1>Quiz</h1>
+  <div v-for="question in JSONfile">
+    <p> {{ question.statement }}</p>
+    <div v-for="(answer, key) in question.answers">
+      <label :for="key">{{ answer }}</label>
+      <input type="radio"
+              :id ="key"
+              :value="key + answer">
+    </div>
+   </div>
+
+
+</template>
+
+
+
+<style scoped>
+</style>
