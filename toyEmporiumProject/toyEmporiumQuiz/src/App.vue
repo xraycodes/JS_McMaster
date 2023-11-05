ed8f5a399cd161083f26bd84e8ee532e8b268815
brianjo94 <brianjo1994@gmail.com>
2023-11-04 23:35:37 -0400

    v-for to iterate over JSON file output as input radio element

--- toyEmporiumProject/toyEmporiumQuiz/src/App.vue
+++ toyEmporiumProject/toyEmporiumQuiz/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import JSONfile from './assets/questions.json'
 
-
+console.log(JSONfile)
 
 
 </script>
@@ -9,15 +9,14 @@ import JSONfile from './assets/questions.json'
 <template>
   <h1>Quiz</h1>
   <div v-for="question in JSONfile">
-    <p> {{ question.statement }}</p>
-    <div v-for="(answer, key) in question.answers">
-      <label :for="key">{{ answer }}</label>
-      <input type="radio"
-              :id ="key"
-              :value="key + answer">
-    </div>
+      <p> {{ question.statement }}</p>
+      <div v-for="(answer, key) in question.answers">
+        <label :for="key">{{ key }} {{ answer }}</label>
+        <input type="radio"
+                :id ="key"
+                :name="'question-' + question.question">
+      </div>
    </div>
-
 
 </template>
 
