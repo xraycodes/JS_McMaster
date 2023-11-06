0c6b418bf29ac432f117144e2af589025d926b77
brianjo94 <brianjo1994@gmail.com>
2023-11-06 12:39:28 -0500

    Using imported components, dynamically adding and removing paragraph on the DOM

--- toyEmporiumProject/toyEmporiumQuiz/src/App.vue
+++ toyEmporiumProject/toyEmporiumQuiz/src/App.vue
@@ -1,14 +1,28 @@
 <script setup>
 import JSONfile from './assets/questions.json'
 
-console.log(JSONfile)
+import {reactive, ref} from 'vue'
+import QuestionTemplate from './components/questionTemplate.vue'
+
+const test = reactive(JSONfile)
+
+const paragraph = ref([])
+const addParagraph = () =>{
+  paragraph.value.push(paragraph.value.length+1)
+}
+
+const removeParagraph = () => {
+  paragraph.value.pop()
+}
+console.log(paragraph.value.length)
+const flag = true
 
 
 </script>
 
 <template>
   <h1>Quiz</h1>
-  <div v-for="question in JSONfile">
+  <div v-if="flag" v-for="question in test">
       <p> {{ question.statement }}</p>
       <div v-for="(answer, key) in question.answers">
         <label :for="key">{{ key }} {{ answer }}</label>
@@ -17,6 +31,19 @@ console.log(JSONfile)
                 :name="'question-' + question.question">
       </div>
    </div>
+
+  <div>
+    <button @click="addParagraph">Click to add template</button>
+    <button @click="removeParagraph">Click to remove template</button>
+    <h1>{{ paragraph }}</h1>
+  
+    
+   
+   <QuestionTemplate />
+
+
+  </div>
+
 
 </template>
 
