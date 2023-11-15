cb1b58f752fd26f481421ee22963cf0fd0a732a5
brianjo94 <brianjo1994@gmail.com>
2023-11-14 20:01:51 -0500

    compositon API practice

---
+++ vue_practice/vue/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import {ref, reactive} from 'vue'
+
+export default {
+  setup() {
+    const name = ref(0)
+
+    return {
+      name
+    }
+  }
+}
+</script>
+
+
+
+
+<template>
+<button @click="name++">Click to add</button>
+<h1> {{ name }}</h1>
+</template>
+
+<style scoped>
+
+</style>
