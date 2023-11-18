64c681f7a9c04e7557cf526cd7ded39317c4ad9a
brianjo94 <brianjo1994@gmail.com>
2023-11-18 18:28:54 -0500

    Learning about components and props

---
+++ vuePractice/vuepracticeFINAL/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import ButtonCounter from './components/ButtonCounter.vue';
+
+const name = 'Bob'
+</script>
+
+
+<template>
+<ButtonCounter :name="name" />
+</template>
+
+
+
+
+<style scoped>
+</style>
---
+++ vuePractice/vuepracticeFINAL/src/components/ButtonCounter.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {ref, reactive} from 'vue'
+
+const button = reactive({
+    count : 0,
+})
+const props = defineProps({
+    name: String
+})
+
+</script>
+
+
+
+
+<template>
+<h1>Hi {{ name }} hows it going</h1>
+<p>Learning about components and props</p><br>
+
+<p> {{ button.count }}</p>
+<button @click="button.count++">Click to increment by 1</button>
+</template>
