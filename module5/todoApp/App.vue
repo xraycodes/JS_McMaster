2d65f515046ae16f1343290e3bb5f97fed7e7833
brianjo94 <brianjo1994@gmail.com>
2023-11-19 21:50:41 -0500

    toDoApp Final using Vue

---
+++ module5/todoApp/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+// Import you custom component(s) here
+import newToDo from './components/newToDo.vue';
+</script>
+
+<template>
+  <header id="container">
+    <h1>Welcome to our ToDo web app</h1>
+    <img src="./assets/todo.png" alt="">
+      <newToDo />
+  </header>
+</template>
+
+<style scoped>
+#container {
+  /* background-color: rgb(81, 169, 241); */
+  padding: 10px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  
+}
+
+#title {
+  color:blue;
+  font-size: 20px;
+}
+</style>
