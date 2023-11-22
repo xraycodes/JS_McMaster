346d202b98a9c8eabd75fa606c2d7f0dfe01e47c
brianjo94 <brianjo1994@gmail.com>
2023-11-21 21:10:37 -0500

    created toDoList and sent prop from parent to child

---
+++ vue-project/src/components/toDoItems.vue
@@ -0,0 +1,86 @@
+<script setup>
+import {ref} from 'vue'
+
+
+defineProps(['toDoListSelection'])
+
+const InputValue = ref('')
+const ToDoItemArray = ref([])
+
+</script>
+
+<template>
+    <div>
+        <div class="container">
+            <h1>ToDo Items: </h1>
+            <button id="newButton" @click="AddInputToList">New</button>
+            <button id="deleteButton">Delete</button>
+        </div>
+
+        <!-- <div v-for="(item,index) in ToDoItemArray">
+            <input type="checkbox" :value="item" :id="index" v-model="toDoListSelection">
+            <label :for="index">{{ item }}</label>
+        </div> -->
+
+
+
+        <div>
+            <input id="input1" type="textbox" v-model="InputValue" required>
+            <label for="input1"> Click New to add to ToDo Item</label>
+        </div>
+
+        <h1>{{ toDoListSelection }}</h1>
+    </div>
+
+
+</template>
+
+<style scoped>
+.container {
+    display:flex
+}
+
+#newButton {
+  background-color: #4c76ea;
+  border-radius: 8px;
+  border-style: none;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-block;
+  font-family: "Haas Grot Text R Web", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  height: 40px;
+  line-height: 20px;
+  list-style: none;
+  margin: 20px;
+  outline: none;
+  padding: 10px 16px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+}   
+
+#deleteButton {
+  background-color: #EA4C89;
+  border-radius: 8px;
+  border-style: none;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-block;
+  font-family: "Haas Grot Text R Web", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  height: 40px;
+  line-height: 20px;
+  list-style: none;
+  margin: 20px;
+  outline: none;
+  padding: 10px 16px;
+  position: relative;
+  text-align: center;
+  text-decoration: none
+}
+</style>
---
+++ vue-project/src/components/toDoList.vue
@@ -0,0 +1,97 @@
+<script setup>
+import toDoItems from './toDoItems.vue'
+import {ref} from 'vue'
+
+
+const InputValue = ref('')
+
+const ToDoListArray = ref([])
+
+const toDoListSelection = ref([])
+
+const AddInputToList = () => {
+    if(InputValue.value){
+        ToDoListArray.value.push(InputValue.value)
+        InputValue.value = ''
+    }
+}
+
+</script>
+
+<template>
+    <div>
+        <div class="container">
+            <h1>ToDo Lists: </h1>
+            <button id="newButton" @click="AddInputToList">New</button>
+            <button id="deleteButton">Delete</button>
+        </div>
+
+        <div v-for="(item,index) in ToDoListArray">
+            <input type="checkbox" :value="item" :id="index" v-model="toDoListSelection">
+            <label :for="index">{{ item }}</label>
+
+        </div>
+
+        <div>
+            <input id="input1" type="textbox" v-model="InputValue" required>
+            <label for="input1"> Click New to add to ToDo List</label>
+        </div>
+    </div>
+
+    <toDoItems :toDoListSelection="toDoListSelection"/>
+
+    <!-- <h1>{{ InputValue }}</h1>
+    <h1>{{ ToDoListArray }}</h1> -->
+    <!-- <h1>{{ toDoListSelection }}</h1> -->
+
+</template>
+
+<style scoped>
+.container {
+    display:flex
+}
+
+#newButton {
+  background-color: #4c76ea;
+  border-radius: 8px;
+  border-style: none;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-block;
+  font-family: "Haas Grot Text R Web", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  height: 40px;
+  line-height: 20px;
+  list-style: none;
+  margin: 20px;
+  outline: none;
+  padding: 10px 16px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+}   
+
+#deleteButton {
+  background-color: #EA4C89;
+  border-radius: 8px;
+  border-style: none;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-block;
+  font-family: "Haas Grot Text R Web", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  height: 40px;
+  line-height: 20px;
+  list-style: none;
+  margin: 20px;
+  outline: none;
+  padding: 10px 16px;
+  position: relative;
+  text-align: center;
+  text-decoration: none
+}
+</style>
